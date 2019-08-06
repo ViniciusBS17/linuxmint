6aaaecc5bab40f64109cf13c927b33e09cf84e6e
Vinicius Bueno <53578170+ViniciusBS17@users.noreply.github.com>
2019-08-06 16:10:38 -0300

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,1 +1,26 @@
 Vinicius Bueno,SENAC Tatuapé, Git 
+ATALHOS:
+
+Atalho para o Terminal (LInux) = Ctrl + ALt + T
+Atalho para o Terminal Puro (Linux) = Ctrl + ALt + F1
+Atalho para voltar = Ctrl + ALt + F7
+
+~ = Diretório pessoal 
+$ = Usuário comum
+LS = Comando de "Listar"
+ * ls -lh = Listar arquivos de diretórios (detalhado)
+Ctrl + L = Limpar
+cd = andar entre diretórios
+cd / = diretório raiz
+cd - = volta para onde estava
+
+CORES
+
+Azul (escuro) = Diretórios
+Azul (claro) = LInk simbólico [atalho]
+Cinza = Arquivos 
+Marca Texto Verde = PERIGO!
+
+pwd = Mostra em qual diretório você está
+mkdir = criação de diretório
+TAB = preencher
